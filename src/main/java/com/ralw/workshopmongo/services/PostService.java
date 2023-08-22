82d07887afe632140b0a4d1a50d7e0172d0582f0
Ricardo Correa de Almeida <ricardo_1402_@hotmail.com>
2023-08-22 16:29:55 -0300

    Obtendo um post por id

---
+++ src/main/java/com/ralw/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.ralw.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ralw.workshopmongo.domain.Post;
+import com.ralw.workshopmongo.repository.PostRepository;
+import com.ralw.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+}
