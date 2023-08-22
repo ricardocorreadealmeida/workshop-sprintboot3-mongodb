792d95046acb6610974c5bbb0d36a07a389e7da6
Ricardo Correa de Almeida <ricardo_1402_@hotmail.com>
2023-08-22 17:08:14 -0300

    Consulta simples com query methods

--- src/main/java/com/ralw/workshopmongo/domain/Post.java
+++ src/main/java/com/ralw/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.ralw.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.ralw.workshopmongo.dto.AuthorDTO;
+import com.ralw.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -20,6 +23,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -71,6 +76,14 @@ public class Post implements Serializable {
 
 	public void setBody(String body) {
 		this.body = body;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
